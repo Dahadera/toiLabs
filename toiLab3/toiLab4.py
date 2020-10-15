8cfb875f87ccab2b376e1ec15872a5e6af195a79
Solja <rdahaderv@gmail.com>
2020-10-16 02:42:03 +0400

    feat: added new styles, added mean-shift and spectral clustering

--- toiLab3/toiLab4.py
+++ toiLab3/toiLab4.py
@@ -3,20 +3,31 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MeanShift, SpectralClustering, estimate_bandwidth
 
-
+# Reading cvs file
 df = pd.read_csv('iris.data-input.cvs')
+# Deleting class column
 df = df.drop('Class', axis=1)
-print(df.head(3))
 processed_df = df.to_numpy()[:, :]
 
-cluster_num = 4
+# K-means clustering
+cluster_num = 3
 k_means = KMeans(init='k-means++', n_clusters=cluster_num, n_init=12)
 k_means.fit(processed_df)
-labels = k_means.labels_
-df['kmean_cluster'] = labels
-print(df.head(3))
+k_means_labels = k_means.labels_
+
+# Mean-shift clustering
+bandwidth = estimate_bandwidth(processed_df, quantile=0.183, n_samples=150)
+m_shift = MeanShift(bandwidth=bandwidth)
+m_shift.fit(processed_df)
+m_shift_labels = m_shift.labels_
+# df['ms_cluster'] = m_shift.labels_
+
+# Spectral clustering
+spectral_c = SpectralClustering(n_clusters=3, assign_labels='discretize', random_state=0)
+spectral_c.fit(processed_df)
+spectral_c_labels = spectral_c.labels_
 
 # Setting up mpl params
 mpl.rcParams['font.family'] = 'Times New Roman'
@@ -24,16 +35,53 @@ mpl.rcParams['axes.unicode_minus'] = False
 rcParams.update({'figure.autolayout': True})
 
 fig = plt.figure()
-# fig = plt.figure(figsize=plt.figaspect(2.))
-ax1 = fig.add_subplot(2, 1, 1)
+# k_means_ax = fig.add_subplot(3, 2, 1)
+# k_means_ax.set_title('a')
+# m_shift_ax = fig.add_subplot(3, 1, 2)
+# m_shift_ax.set_title('y')
+# spectral_c_ax = fig.add_subplot(3, 1, 3)
+# spectral_c_ax.set_title('ee')
+# https://stackoverflow.com/questions/40936729/matplotlib-title-spanning-two-or-any-number-of-subplot-columns
 
-ax1.scatter(processed_df[:, 0], processed_df[:, 1], c=labels.astype(np.float), alpha=0.8)
-ax1.set_xlabel('Sepal length', fontsize=16)
-ax1.set_ylabel('Sepal width', fontsize=16)
+colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', k_means_labels))
 
-ax2 = fig.add_subplot(2, 1, 2)
-ax2.scatter(processed_df[:, 2], processed_df[:, 3], c=labels.astype(np.float), alpha=0.8)
-ax2.set_xlabel('Petal length', fontsize=16)
-ax2.set_ylabel('Petal width', fontsize=16)
+ax1 = fig.add_subplot(3, 2, 1)
+ax1.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
+# ax1.set_title('K-means')
+ax1.set_xlabel('Sepal length', fontsize=14)
+ax1.set_ylabel('Sepal width', fontsize=14)
+
+ax2 = fig.add_subplot(3, 2, 2)
+ax2.scatter(processed_df[:, 2], processed_df[:, 3], c=colors, alpha=0.8)
+ax2.set_xlabel('Petal length', fontsize=14)
+ax2.set_ylabel('Petal width', fontsize=14)
+
+
+colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', m_shift_labels))
+
+ax3 = fig.add_subplot(3, 2, 3)
+ax3.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
+# ax3.set_title('Mean-shift')
+ax3.set_xlabel('Sepal length', fontsize=14)
+ax3.set_ylabel('Sepal width', fontsize=14)
+
+ax4 = fig.add_subplot(3, 2, 4)
+ax4.scatter(processed_df[:, 2], processed_df[:, 3], c=colors, alpha=0.8)
+ax4.set_xlabel('Petal length', fontsize=14)
+ax4.set_ylabel('Petal width', fontsize=14)
+
+colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', spectral_c_labels))
+
+ax5 = fig.add_subplot(3, 2, 5)
+ax5.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
+# ax3.set_title('Spectral clustering')
+ax5.set_xlabel('Sepal length', fontsize=14)
+ax5.set_ylabel('Sepal width', fontsize=14)
+
+ax6 = fig.add_subplot(3, 2, 6)
+ax6.scatter(processed_df[:, 2], processed_df[:, 3], c=colors, alpha=0.8)
+ax6.set_xlabel('Petal length', fontsize=14)
+ax6.set_ylabel('Petal width', fontsize=14)
+
 
 plt.show()
