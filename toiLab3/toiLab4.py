2f7d8e5f3fd2fb487da69a10cd6610bf333f15e4
Solja <rdahaderv@gmail.com>
2020-10-16 12:51:07 +0400

    feat: titles added

--- toiLab3/toiLab4.py
+++ toiLab3/toiLab4.py
@@ -34,20 +34,13 @@ mpl.rcParams['font.family'] = 'Times New Roman'
 mpl.rcParams['axes.unicode_minus'] = False
 rcParams.update({'figure.autolayout': True})
 
-fig = plt.figure()
-# k_means_ax = fig.add_subplot(3, 2, 1)
-# k_means_ax.set_title('a')
-# m_shift_ax = fig.add_subplot(3, 1, 2)
-# m_shift_ax.set_title('y')
-# spectral_c_ax = fig.add_subplot(3, 1, 3)
-# spectral_c_ax.set_title('ee')
-# https://stackoverflow.com/questions/40936729/matplotlib-title-spanning-two-or-any-number-of-subplot-columns
+fig = plt.figure(figsize=(12, 8))
 
 colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', k_means_labels))
+plt.figtext(0.5, 0.9885, 'K-means', va='center', ha='center', size=12, fontweight='bold')
 
 ax1 = fig.add_subplot(3, 2, 1)
 ax1.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
-# ax1.set_title('K-means')
 ax1.set_xlabel('Sepal length', fontsize=14)
 ax1.set_ylabel('Sepal width', fontsize=14)
 
@@ -58,10 +51,10 @@ ax2.set_ylabel('Petal width', fontsize=14)
 
 
 colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', m_shift_labels))
+plt.figtext(0.5, 0.681, 'Mean-shift', va='center', ha='center', size=13, fontweight='bold')
 
 ax3 = fig.add_subplot(3, 2, 3)
 ax3.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
-# ax3.set_title('Mean-shift')
 ax3.set_xlabel('Sepal length', fontsize=14)
 ax3.set_ylabel('Sepal width', fontsize=14)
 
@@ -71,10 +64,10 @@ ax4.set_xlabel('Petal length', fontsize=14)
 ax4.set_ylabel('Petal width', fontsize=14)
 
 colors = list(map(lambda x: '#3b4cc0' if x == 1 else '#b40426' if x == 2 else '#67c614', spectral_c_labels))
+plt.figtext(0.5, 0.350, 'Spectral clustering', va='center', ha='center', size=13, fontweight='bold')
 
 ax5 = fig.add_subplot(3, 2, 5)
 ax5.scatter(processed_df[:, 0], processed_df[:, 1], c=colors, alpha=0.8)
-# ax3.set_title('Spectral clustering')
 ax5.set_xlabel('Sepal length', fontsize=14)
 ax5.set_ylabel('Sepal width', fontsize=14)
 
@@ -82,6 +75,5 @@ ax6 = fig.add_subplot(3, 2, 6)
 ax6.scatter(processed_df[:, 2], processed_df[:, 3], c=colors, alpha=0.8)
 ax6.set_xlabel('Petal length', fontsize=14)
 ax6.set_ylabel('Petal width', fontsize=14)
-
 
 plt.show()
